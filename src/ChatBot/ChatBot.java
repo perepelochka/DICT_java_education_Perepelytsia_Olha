505bf791bc9873ee9ebfab3e6246e735ae3b5a83
perepelochka <o.o.perepelytsia@student.khai.edu>
2021-11-14 20:15:22 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,17 @@
-package ChatBot;public class ChatBot {
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String bot_name = "WALL-E";
+        int birth_year = 2021;
+        System.out.println("Hello! My name is "+bot_name+".\n"+"I was created in "+birth_year+".");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, remind me your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String user_name = scanner.nextLine();
+        System.out.println("What a great name you have, "+ user_name+"!");
+
+    }
 }
