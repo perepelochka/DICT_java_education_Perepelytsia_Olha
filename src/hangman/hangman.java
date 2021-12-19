a17c2db021103caaf95a67d0f45fdfe5ef206ff8
perepelochka <o.o.perepelytsia@student.khai.edu>
2021-12-19 14:40:56 +0200

    2-nd commit

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,2 +1,26 @@
-package hangman;public class hangman {
+
+package hangman;
+import java.util.Scanner;
+
+public class hangman {
+
+    public static void main(String[] args) {
+        System.out.println("HANGMAN\n" + "The game will be available soon.");
+
+        String words = "java";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Guess the word: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String user_input = scanner.nextLine();
+
+            if (words.equals(user_input)) {
+                System.out.println("You survived!");
+
+            } else {
+                System.out.println("You lost!:(");
+            }
+    }
+
+
 }
